958202ab66b076954b4812cdbeb0dfbd09f59f7f
kmal808 <kjmalley7@gmail.com>
2022-06-23 23:59:54 -1000

    home screen and nav bar

--- next.config.js
+++ next.config.js
@@ -3,4 +3,23 @@ const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = nextConfig
+module.exports = {
+  images: {
+    domains: [
+      'media-exp2.licdn.com',
+      'cdn-icons-png.flaticon.com',
+      'github.com',
+      'images.unsplash.com',
+      'live.staticflickr.com',
+      'drive.google.com',
+      'img.freepik.com',
+      'iconape.com',
+      'banner2.cleanpng.com',
+      'w7.pngwing.com',
+      'e7.pngegg.com',
+      'banner2.cleanpng.com',
+      'img.icons8.com',
+      'mpng.subpng.com',
+    ],
+  },
+}
