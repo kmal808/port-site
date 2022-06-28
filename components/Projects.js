11ae530a679b90441d31981a90471e2d89f8ad67
kmal808 <kjmalley7@gmail.com>
2022-06-27 22:57:18 -1000

    added projects and created projectItems componenent

--- components/Projects.js
+++ components/Projects.js
@@ -1,15 +1,45 @@
 import React from 'react'
+import Link from 'next/link'
+import Image from 'next/image'
+import bankiImage from '../public/assets/projects/banki-node-app.gif'
+import mernEcom from '../public/assets/projects/mern-ecom.gif'
+import bigWaveTracker from '../public/assets/projects/big-wave-tracker.gif'
+import wesMansionTattoos from '../public/assets/projects/wes-mansion-tattos.gif'
+import ProjectItem from './ProjectItem'
 
 function Projects() {
   return (
     <div className='w-full'>
-      <div className='m-w-[1240px] mx-auto px-2 py-16'>
+      <div className='max-w-[1240px] mx-auto px-2 py-16'>
         <p className='text-xl tracking-widest uppercase text-teal-500'>
           Projects
         </p>
         <h2 className='py-4'>What I've Built</h2>
         <div className='grid md:grid-col-2 gap-8'>
-          <div className='relative flex items-center justify-self-center h-auto w-full shadow-xl shadow-teal-400 rounded-xl p-4 group hover:bg-gradient-to-r from-teal-300 to-teal-100'></div>
+          <ProjectItem
+            title='Technical Interview Prep'
+            backgroundImg={bankiImage}
+            projectUrl='/banki'
+            tech='VanillaJS, Express'
+          />
+          <ProjectItem
+            title='Mern Ecommerce'
+            backgroundImg={mernEcom}
+            projectUrl='/mern'
+            tech='ReactJS, Express, MongoDB'
+          />
+          <ProjectItem
+            title='Big Wave Tracker'
+            backgroundImg={bigWaveTracker}
+            projectUrl='/bigwave'
+            tech='HTML, CSS'
+          />
+          <ProjectItem
+            title='Wes Mansion Tattos'
+            backgroundImg={wesMansionTattoos}
+            projectUrl='/tattoos'
+            tech='HTML, CSS'
+          />
         </div>
       </div>
     </div>
