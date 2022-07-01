cd12072dd6aba5fcdc592dc6f9638ea16dd2ad7e
kmal808 <kjmalley7@gmail.com>
2022-06-30 22:31:57 -1000

    added links to buttons

--- pages/banki.jsx
+++ pages/banki.jsx
@@ -36,8 +36,20 @@ const banki = () => {
             upon. This was originally written in vanilla node but then later
             wasa rewritten using Express.
           </p>
-          <button className='px-8 py-2 mt-4 mr-8'>Demo</button>
-          <button className='px-8 py-2 mt-4'>Code</button>
+          <a
+            href='https://banki-node-app.herokuapp.com'
+            target='_blank'
+            rel='noreferrer'
+          >
+            <button className='px-8 py-2 mt-4 mr-8'>Demo</button>
+          </a>
+          <a
+            href='https://github.com/kmal808/banki-node-app'
+            target='_blank'
+            rel='noreferrer'
+          >
+            <button className='px-8 py-2 mt-4'>Code</button>
+          </a>
         </div>
         <div className='col-span-4 md:col-span-1 shadow-xl shadow-gray-400 rounded-xl p-4'>
           <div className='p-2'>
