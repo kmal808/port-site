919d3c8bec0dc26db405850502be5fdbaa7d8fe0
kmal808 <kjmalley7@gmail.com>
2022-07-02 23:30:41 -1000

    fixed margin/layout on about and padding on skills headings

--- components/About.jsx
+++ components/About.jsx
@@ -4,63 +4,51 @@ import Image from 'next/image'
 import Profile from '/home/kurt/code/repos/port-site/public/assets/profile.jpg'
 
 const About = () => {
-  return (
-    <div id='about' className='w-full md:h-screen p-2 flex items-center py-16'>
-      <div className='max-w[1240px] m-auto md:grid grid-cols-3 gap-8'>
-        <div className='col-span-2'>
-          <p className='uppercase text-xl tracking-widest text-teal-500'>
-            About
-          </p>
-          <h1 className='py-6'>Who - What - When - Where - How</h1>
-          <h2 className='py-4'>Who I Am</h2>
-          <p className='py-2 text-gray-600'>I am not your average developer</p>
-          <p className='py-2 text-gray-600'>
-            🏗️ Highly experienced construction operations manager that has
-            brought my passion, problem solving and collaborative skills to
-            field of software engineering.
-          </p>
-          <h3 className='py-4'>Where I'm At</h3>
-          <p className='py-2 text-gray-600'>
-            🌎 Based on the island of Oahu, Hawaii
-          </p>
-          <h4 className='py-4'>What I Do</h4>
-          <p className='py-2 text-gray-600'>
-            {' '}
-            //Todo finish what I do 🦺 After a decade of managing construction
-            projects, experience .......... Lorem ipsum dolor sit amet
-            consectetur adipisicing elit. Suscipit, odit a? Natus quo laboriosam
-            accusamus exercitationem soluta quia dolorem est! Quos eius possimus
-            voluptatibus, sint omnis maiores eligendi cupiditate repellendus.
-          </p>
-          <h5 className='py-4'>How I Do It</h5>
-          <p className='py-2 text-gray-600'>
-            {' '}
-            //Todo finish how I do it 💪 My father taught me from a young age
-            that hard work pays off, and don't do a job halfa$$. Because of that
-            my work ethic and dedication are unmatched ....... Lorem, ipsum
-            dolor sit amet consectetur adipisicing elit. Consectetur, enim sint
-            doloribus aut similique repellendus ut a deleniti nisi dolore natus
-            blanditiis? Labore aliquam quia quibusdam obcaecati perferendis!
-            Consequuntur, quam.
-          </p>
-          <Link href='/#projects'>
-            <p className='py-2 text-gray-600 underline cursor-pointer'>
-              👀 Check out some of my latest projects...
-            </p>
-          </Link>
-        </div>
-        <div className='w-full h-auto m-auto shadow-xl shadow-teal-500 rounded-full flex items-center justify-center p-4 hover:scale-105 ease-in duration-300'>
-          <Image
-            className='rounded-full'
-            src={Profile}
-            alt='me'
-            width={500}
-            height={500}
-          />
-        </div>
-      </div>
-    </div>
-  )
+	return (
+		<div id='about' className='w-full md:h-screen p-2 flex items-center py-16'>
+			<div className='max-w-[1240px] m-auto md:grid grid-cols-3 gap-8'>
+				<div className='col-span-2'>
+					<p className='uppercase text-xl tracking-widest text-teal-500'>
+						About
+					</p>
+					<h2 className='py-4'>Who - What - When - Where - How</h2>
+					<h4 className='py-4'>Who I Am</h4>
+					<p className='py-2 text-gray-600'>
+						🏗️ Highly experienced construction operations manager that has
+						brought my passion, problem solving and collaborative skills to
+						field of software engineering.
+					</p>
+					<h4 className='py-4'>Where I'm At</h4>
+					<p className='py-2 text-gray-600'>
+						🌎 Based on the island of Oahu, Hawaii
+					</p>
+					<h4 className='py-4'>What I Do</h4>
+					<p className='py-2 text-gray-600'>
+						{/* ToDo finish section  */}
+						🦺 After a decade of managing construction projects, experience
+						.......... Lorem ipsum dolor sit amet consectetur adipisicing elit.
+						Suscipit, odit a? blah blah blah blah blah.
+					</p>
+					<h4 className='py-4'>How I Do It</h4>
+					<p className='py-2 text-gray-600'>
+						{/* ToDo finish section  */}
+						💪 My father taught me from a young age that hard work pays off, and
+						to never do a job halfa$$. Because of that my work ethic and
+						dedication are unmatched ....... Lorem, ipsum dolor sit amet
+						consectetur.
+					</p>
+					<Link href='/#projects'>
+						<p className='py-2 text-gray-600 underline cursor-pointer'>
+							👀 Check out some of my latest projects...
+						</p>
+					</Link>
+				</div>
+				<div className='w-full h-auto m-auto shadow-xl shadow-teal-500 rounded-full flex items-center justify-center p-4 hover:scale-105 ease-in duration-300'>
+					<Image className='rounded-full' src={Profile} alt='me' />
+				</div>
+			</div>
+		</div>
+	)
 }
 
 export default About
--- components/Skills.jsx
+++ components/Skills.jsx
@@ -2,195 +2,195 @@ import React from 'react'
 import Image from 'next/image'
 
 const Skills = () => {
-  return (
-    <div id='skills' className='w-full p-2 lg:h-screen'>
-      <div className='max-w-[1240px] mx-auto flex flex-col justify-center h-full'>
-        <p className='text-xl py-10 tracking-widest text-teal-500 uppercase'>
-          Skills
-        </p>
-        <h2 className='py-4'>What I Can Do</h2>
+	return (
+		<div id='skills' className='w-full p-2 lg:h-screen'>
+			<div className='max-w-[1240px] mx-auto flex flex-col justify-center h-full'>
+				<p className='text-xl tracking-widest text-teal-500 uppercase'>
+					Skills
+				</p>
+				<h2 className='py-4'>What I Can Do</h2>
 
-        <div className='grid gap-8 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 '>
-          <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
-            <div className='grid items-center justify-center grid-cols-2 gap-4'>
-              <div className='m-auto'>
-                <Image
-                  src='https://github.com/fireclint/portfolio-nextjs/blob/main/public/assets/skills/css.png?raw=true'
-                  width='64px'
-                  height='64px'
-                  alt='github'
-                />
-              </div>
-              <div className='flex flex-col items-center justify-center'>
-                <h3>CSS</h3>
-              </div>
-            </div>
-          </div>
+				<div className='grid gap-8 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 '>
+					<div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
+						<div className='grid items-center justify-center grid-cols-2 gap-4'>
+							<div className='m-auto'>
+								<Image
+									src='https://github.com/fireclint/portfolio-nextjs/blob/main/public/assets/skills/css.png?raw=true'
+									width='64px'
+									height='64px'
+									alt='github'
+								/>
+							</div>
+							<div className='flex flex-col items-center justify-center'>
+								<h3>CSS</h3>
+							</div>
+						</div>
+					</div>
 
-          <div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
-            <div className='grid items-center justify-center grid-cols-2 gap-4'>
-              <div className='m-auto'>
-                <Image
-                  src='https://github.com/fireclint/portfolio-nextjs/blob/main/public/assets/skills/html.png?raw=true'
-                  width='64px'
-                  height='64px'
-                  alt='github'
-                />
-              </div>
-              <div className='flex flex-col items-center justify-center'>
-                <h3>HTML</h3>
-              </div>
-            </div>
-          </div>
+					<div className='p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
+						<div className='grid items-center justify-center grid-cols-2 gap-4'>
+							<div className='m-auto'>
+								<Image
+									src='https://github.com/fireclint/portfolio-nextjs/blob/main/public/assets/skills/html.png?raw=true'
+									width='64px'
+									height='64px'
+									alt='github'
+								/>
+							</div>
+							<div className='flex flex-col items-center justify-center'>
+								<h3>HTML</h3>
+							</div>
+						</div>
+					</div>
 
-          <div className='p-5 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
-            <div className='grid grid-cols-2 gap-4 justify-center items-center'>
-              <div className='m-auto'>
-                <Image
-                  src='https://github.com/fireclint/portfolio-nextjs/blob/main/public/assets/skills/javascript.png?raw=true'
-                  width='64px'
-                  height='64px'
-                  alt='/'
-                />
-              </div>
-              <div className='flex flex-col items-center justify-center'>
-                <h3>JavaScript</h3>
-              </div>
-            </div>
-          </div>
+					<div className='p-5 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
+						<div className='grid grid-cols-2 gap-4 justify-center items-center'>
+							<div className='m-auto'>
+								<Image
+									src='https://github.com/fireclint/portfolio-nextjs/blob/main/public/assets/skills/javascript.png?raw=true'
+									width='64px'
+									height='64px'
+									alt='/'
+								/>
+							</div>
+							<div className='flex flex-col items-center justify-center'>
+								<h3>JavaScript</h3>
+							</div>
+						</div>
+					</div>
 
-          <div className='p-5 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
-            <div className='grid items-center justify-center grid-cols-2 gap-4'>
-              <div className='m-auto'>
-                <Image
-                  src='https://github.com/fireclint/portfolio-nextjs/blob/main/public/assets/skills/node.png?raw=true'
-                  width='64px'
-                  height='64px'
-                  alt='github'
-                />
-              </div>
-              <div className='flex flex-col items-center justify-center'>
-                <h3>Node.JS</h3>
-              </div>
-            </div>
-          </div>
+					<div className='p-5 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
+						<div className='grid items-center justify-center grid-cols-2 gap-4'>
+							<div className='m-auto'>
+								<Image
+									src='https://github.com/fireclint/portfolio-nextjs/blob/main/public/assets/skills/node.png?raw=true'
+									width='64px'
+									height='64px'
+									alt='github'
+								/>
+							</div>
+							<div className='flex flex-col items-center justify-center'>
+								<h3>Node.JS</h3>
+							</div>
+						</div>
+					</div>
 
-          <div className='p-5 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
-            <div className='grid items-center justify-center grid-cols-2 gap-4'>
-              <div className='m-auto'>
-                <Image
-                  src='
+					<div className='p-5 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
+						<div className='grid items-center justify-center grid-cols-2 gap-4'>
+							<div className='m-auto'>
+								<Image
+									src='
                   https://iconape.com/wp-content/files/gm/82643/svg/next-js.svg'
-                  width='64px'
-                  height='64px'
-                  alt='github'
-                />
-              </div>
-              <div className='flex flex-col items-center justify-center'>
-                <h3>Next.JS</h3>
-              </div>
-            </div>
-          </div>
+									width='64px'
+									height='64px'
+									alt='github'
+								/>
+							</div>
+							<div className='flex flex-col items-center justify-center'>
+								<h3>Next.JS</h3>
+							</div>
+						</div>
+					</div>
 
-          <div className='p-5 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
-            <div className='grid items-center justify-center grid-cols-2 gap-4'>
-              <div className='m-auto'>
-                <Image
-                  src='https://github.com/fireclint/portfolio-nextjs/blob/main/public/assets/skills/react.png?raw=true'
-                  width='64px'
-                  height='64px'
-                  alt='github'
-                />
-              </div>
-              <div className='flex flex-col items-center justify-center'>
-                <h3>React</h3>
-              </div>
-            </div>
-          </div>
+					<div className='p-5 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
+						<div className='grid items-center justify-center grid-cols-2 gap-4'>
+							<div className='m-auto'>
+								<Image
+									src='https://github.com/fireclint/portfolio-nextjs/blob/main/public/assets/skills/react.png?raw=true'
+									width='64px'
+									height='64px'
+									alt='github'
+								/>
+							</div>
+							<div className='flex flex-col items-center justify-center'>
+								<h3>React</h3>
+							</div>
+						</div>
+					</div>
 
-          <div className='p-5 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
-            <div className='grid items-center justify-center grid-cols-2 gap-4'>
-              <div className='m-auto'>
-                <Image
-                  src='https://github.com/fireclint/portfolio-nextjs/blob/main/public/assets/skills/github1.png?raw=true'
-                  width='64px'
-                  height='64px'
-                  alt='github'
-                />
-              </div>
-              <div className='flex flex-col items-center justify-center'>
-                <h3>GitHub</h3>
-              </div>
-            </div>
-          </div>
+					<div className='p-5 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
+						<div className='grid items-center justify-center grid-cols-2 gap-4'>
+							<div className='m-auto'>
+								<Image
+									src='https://github.com/fireclint/portfolio-nextjs/blob/main/public/assets/skills/github1.png?raw=true'
+									width='64px'
+									height='64px'
+									alt='github'
+								/>
+							</div>
+							<div className='flex flex-col items-center justify-center'>
+								<h3>GitHub</h3>
+							</div>
+						</div>
+					</div>
 
-          <div className='p-5 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
-            <div className='grid items-center justify-center grid-cols-2 gap-4'>
-              <div className='m-auto'>
-                <Image
-                  src='https://github.com/fireclint/portfolio-nextjs/blob/main/public/assets/skills/node.png?raw=true'
-                  width='64px'
-                  height='64px'
-                  alt='github'
-                />
-              </div>
-              <div className='flex flex-col items-center justify-center'>
-                <h3>Express</h3>
-              </div>
-            </div>
-          </div>
+					<div className='p-5 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
+						<div className='grid items-center justify-center grid-cols-2 gap-4'>
+							<div className='m-auto'>
+								<Image
+									src='https://github.com/fireclint/portfolio-nextjs/blob/main/public/assets/skills/node.png?raw=true'
+									width='64px'
+									height='64px'
+									alt='github'
+								/>
+							</div>
+							<div className='flex flex-col items-center justify-center'>
+								<h3>Express</h3>
+							</div>
+						</div>
+					</div>
 
-          <div className='p-5 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
-            <div className='grid items-center justify-center grid-cols-2 gap-4'>
-              <div className='m-auto'>
-                <Image
-                  src='https://github.com/fireclint/portfolio-nextjs/blob/main/public/assets/skills/mongo.png?raw=true'
-                  width='64px'
-                  height='64px'
-                  alt='github'
-                />
-              </div>
-              <div className='flex flex-col items-center justify-center'>
-                <h3>MongoDB</h3>
-              </div>
-            </div>
-          </div>
+					<div className='p-5 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
+						<div className='grid items-center justify-center grid-cols-2 gap-4'>
+							<div className='m-auto'>
+								<Image
+									src='https://github.com/fireclint/portfolio-nextjs/blob/main/public/assets/skills/mongo.png?raw=true'
+									width='64px'
+									height='64px'
+									alt='github'
+								/>
+							</div>
+							<div className='flex flex-col items-center justify-center'>
+								<h3>MongoDB</h3>
+							</div>
+						</div>
+					</div>
 
-          <div className='p-5 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
-            <div className='grid items-center justify-center grid-cols-2 gap-4'>
-              <div className='m-auto'>
-                <Image
-                  src='https://github.com/fireclint/portfolio-nextjs/blob/main/public/assets/skills/tailwind.png?raw=true'
-                  width='64px'
-                  height='64px'
-                  alt='github'
-                />
-              </div>
-              <div className='flex flex-col items-center justify-center'>
-                <h3>Tailwind</h3>
-              </div>
-            </div>
-          </div>
+					<div className='p-5 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
+						<div className='grid items-center justify-center grid-cols-2 gap-4'>
+							<div className='m-auto'>
+								<Image
+									src='https://github.com/fireclint/portfolio-nextjs/blob/main/public/assets/skills/tailwind.png?raw=true'
+									width='64px'
+									height='64px'
+									alt='github'
+								/>
+							</div>
+							<div className='flex flex-col items-center justify-center'>
+								<h3>Tailwind</h3>
+							</div>
+						</div>
+					</div>
 
-          <div className='p-5 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
-            <div className='grid items-center justify-center grid-cols-2 gap-4'>
-              <div className='m-auto'>
-                <Image
-                  src='https://github.com/fireclint/portfolio-nextjs/blob/main/public/assets/skills/tailwind.png?raw=true'
-                  width='64px'
-                  height='64px'
-                  alt='github'
-                />
-              </div>
-              <div className='flex flex-col items-center justify-center'>
-                <h3>Bootstrap</h3>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  )
+					<div className='p-5 shadow-xl rounded-xl hover:scale-105 ease-in duration-300'>
+						<div className='grid items-center justify-center grid-cols-2 gap-4'>
+							<div className='m-auto'>
+								<Image
+									src='https://github.com/fireclint/portfolio-nextjs/blob/main/public/assets/skills/tailwind.png?raw=true'
+									width='64px'
+									height='64px'
+									alt='github'
+								/>
+							</div>
+							<div className='flex flex-col items-center justify-center'>
+								<h3>Bootstrap</h3>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	)
 }
 
 export default Skills
