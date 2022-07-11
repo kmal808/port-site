51bed5f6407d15ffad08c075f66b9f10b7a7d16a
kmal808 <kjmalley7@gmail.com>
2022-07-10 21:24:00 -1000

    fixed form, now functional

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -8,18 +8,35 @@ import { BsFillPersonLinesFill } from 'react-icons/bs'
 import { HiOutlineChevronDoubleUp } from 'react-icons/hi'
 
 const Contact = () => {
-	const [name, setName] = useState('')
-	const [phone, setPhone] = useState('')
-	const [email, setEmail] = useState('')
-	const [subject, setSubject] = useState('')
-	const [message, setMessage] = useState('')
+	const [query, setQuery] = useState({
+		name: '',
+		phone: '',
+		email: '',
+		subject: '',
+	})
 
-	const handleSubmit = () => {
-		setName('')
-		setPhone('')
-		setEmail('')
-		setSubject('')
-		setMessage('')
+	// Update inputs value
+	const handleParam = () => (e) => {
+		const name = e.target.name
+		const value = e.target.value
+		setQuery((prevState) => ({
+			...prevState,
+			[name]: value,
+		}))
+	}
+	// Form Submit function
+	const formSubmit = (e) => {
+		e.preventDefault()
+		const formData = new FormData()
+		Object.entries(query).forEach(([key, value]) => {
+			formData.append(key, value)
+		})
+		fetch('https://getform.io/f/5049ea6d-d28c-4dd2-b0e1-fc9e7027e71e', {
+			method: 'POST',
+			body: formData,
+		}).then(() =>
+			setQuery({ name: '', phone: '', email: '', subject: '', message: '' })
+		)
 	}
 
 	return (
@@ -92,11 +109,7 @@ const Contact = () => {
 					{/* right */}
 					<div className='col-span-3 w-full h-auto shadow-xl shadow-gray-400 rounded-xl lg:p-4'>
 						<div className='p-4'>
-							<form
-								onSubmit={handleSubmit}
-								action='https://getform.io/f/5049ea6d-d28c-4dd2-b0e1-fc9e7027e71e'
-								method='POST'
-							>
+							<form onSubmit={formSubmit}>
 								<div className='grid md:grid-cols-2 gap-4 w-full py-2'>
 									<div className='flex flex-col'>
 										<label className='uppercase text-sm py-2'>Name</label>
@@ -104,8 +117,9 @@ const Contact = () => {
 											className='border-2 rounded-lg p-3 flex border-gray-300'
 											type='text'
 											name='name'
-											value={name}
-											onChange={(e) => setName(e.target.value)}
+											required
+											value={query.name}
+											onChange={handleParam()}
 										/>
 									</div>
 									<div className='flex flex-col'>
@@ -116,8 +130,8 @@ const Contact = () => {
 											className='border-2 rounded-lg p-3 flex border-gray-300'
 											type='text'
 											name='phone'
-											value={phone}
-											onChange={(e) => setPhone(e.target.value)}
+											value={query.phone}
+											onChange={handleParam()}
 										/>
 									</div>
 								</div>
@@ -127,18 +141,20 @@ const Contact = () => {
 										className='border-2 rounded-lg p-3 flex border-gray-300'
 										type='email'
 										name='email'
-										value={email}
-										onChange={(e) => setEmail(e.target.value)}
+										required
+										value={query.email}
+										onChange={handleParam()}
 									/>
 								</div>
 								<div className='flex flex-col py-2'>
 									<label className='uppercase text-sm py-2'>Subject</label>
 									<input
 										className='border-2 rounded-lg p-3 flex border-gray-300'
-										type='text'
+										type='subject'
 										name='subject'
-										value={subject}
-										onChange={(e) => setSubject(e.target.value)}
+										required
+										value={query.subject}
+										onChange={handleParam()}
 									/>
 								</div>
 								<div className='flex flex-col py-2'>
@@ -147,11 +163,15 @@ const Contact = () => {
 										className='border-2 rounded-lg p-3 border-gray-300'
 										rows='10'
 										name='message'
-										value={message}
-										onChange={(e) => setMessage(e.target.value)}
+										required
+										value={query.message}
+										onChange={handleParam()}
 									></textarea>
 								</div>
-								<button className='w-full p-4 text-gray-100 mt-4 shadow-lg shadow-gray-400 rounded-xl uppercase bg-gradient-to-r from-teal-300 to-teal-600'>
+								<button
+									type='submit'
+									className='w-full p-4 text-gray-100 mt-4 shadow-lg shadow-gray-400 rounded-xl uppercase bg-gradient-to-r from-teal-300 to-teal-600'
+								>
 									Send Message
 								</button>
 							</form>
