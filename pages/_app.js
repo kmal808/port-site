f54576b2934f7dcf23824daf944516e0132cdb03
kmal808 <kjmalley7@gmail.com>
2022-07-10 23:00:39 -1000

    made dark the default theme

--- pages/_app.js
+++ pages/_app.js
@@ -6,7 +6,11 @@ import { ThemeProvider } from 'next-themes'
 function MyApp({ Component, pageProps }) {
 	return (
 		<>
-			<ThemeProvider attribute='class' enableSystem={true}>
+			<ThemeProvider
+				attribute='class'
+				enableSystem={true}
+				defaultTheme={'dark'}
+			>
 				<Nav />
 				<Component {...pageProps} />
 			</ThemeProvider>
